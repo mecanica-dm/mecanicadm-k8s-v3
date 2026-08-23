3cccae2a176b36bf824bc25e60d2baad6089e4d8
Diego Priess <diegopriess.dev@gmail.com>
2026-08-23 00:05:58 -0300

    fix(cluster): Ajuste erro ao tentar acessar dados de cluster que ainda não existe. Considerava cluster sendo criado em outro local

--- providers.tf
+++ providers.tf
@@ -31,24 +31,20 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
