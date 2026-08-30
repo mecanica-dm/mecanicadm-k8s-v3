f824bfff25d94d2caa885f3d342f50e4f204f6f2
Diego Priess <diegopriess.dev@gmail.com>
2026-08-29 22:50:01 -0300

    fix(policy_arn): Ajustando politica de arn

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "eks" {
       principal_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       policy_associations = {
         admin = {
-          policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterAdminPolicy"
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             type = "cluster"
           }
